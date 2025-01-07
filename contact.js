2734412c99f6f9bb29a79cd8091dd4cf32b6e775
kmizuo <kmizuo@uci.edu>
2025-01-07 14:46:16 -0800

    Giving up on AJAX and just sticking to sending the output to a hidden iframe.

--- contact.js
+++ contact.js
@@ -1,32 +1,4 @@
 $('#contact_form').submit(function (e) {
-
-    e.preventDefault(); // prevent from submitting form directly
-
-    jQuery.ajax({
-        url: 'https://docs.google.com/forms/u/2/d/e/1FAIpQLScYDf5PRl5UI07sXEaGJaugq7foo4DbfQfJ9N6j2YLwN6ifAA/formResponse',
-        method: 'POST',
-        headers: { 'Access-Control-Allow-Origin': 'https://docs.google.com' },
-        data: $('#contact_form').serializeArray()
-    })
-        .done(function (response) {
-            alert(response);
-            $('#contact_form').reset();
-        })
-        .fail(function (error) {
-            alert(error);
-        })
-
-    // $.ajax({
-    //   url: 'form.php',
-    //   method: 'post',
-    //   data: $("#myForm").serializeArray() // convert all form data to array (key:value)
-    // })
-    // .done(function(response){
-    //   alert(response); // show the response
-    //   $("#myForm").reset(); // reset the form
-    // })
-    // .fail(function(error){
-    //   alert(error); // show the error.
-    // });
-
+    document.getElementById('submission').setAttribute('src', '');
+    alert("Thank you for reaching out! I'll get back to you as soon as possible.");
 })
