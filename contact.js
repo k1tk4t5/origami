8d2170d057490f92a3ea50798fe4e62c6b3513ea
k1tk4t5 <akemi90638@gmail.com>
2025-06-04 15:42:00 -0700

    Updating contact page to embed new contact form

--- contact.js
+++
@@ -1,4 +0,0 @@
-$('#contact_form').submit(function (e) {
-    document.getElementById('submission').setAttribute('src', '');
-    alert("Thank you for reaching out! I'll get back to you as soon as possible.");
-})
